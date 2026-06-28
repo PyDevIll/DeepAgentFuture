139561ff5db310f7c4c181b29df25576d40effa0
PyDevIll <delphadyth@gmail.com>
2026-06-28 16:31:46 +0700

    Trying to integrate rest_api_tool

--- src/deep_agent_future/builtin_tools/__init__.py
+++ src/deep_agent_future/builtin_tools/__init__.py
@@ -4,7 +4,7 @@ from loguru import logger
 
 
 def register_all(registry):
-    from . import fs_tools, search_tools, git_tools, tavily_tools, meta_tools, edit_tools, telegram_tools, groq_whisper_tools, tts_tools, additional_tools
+    from . import fs_tools, search_tools, git_tools, tavily_tools, meta_tools, edit_tools, telegram_tools, groq_whisper_tools, tts_tools, additional_tools, rest_api_tool
     fs_tools.register_all(registry)
     search_tools.register_all(registry)
     git_tools.register_all(registry)
@@ -15,4 +15,5 @@ def register_all(registry):
     groq_whisper_tools.register_all(registry)
     tts_tools.register_all(registry)
     additional_tools.register_all(registry)
+    rest_api_tool.register_all(registry)
     logger.info("All builtin tools registered")
